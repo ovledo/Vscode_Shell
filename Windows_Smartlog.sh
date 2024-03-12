fe051a4a298ef8cd63fcbe00333e6ed97d1a1296
ovledo <932816635@qq.com>
2024-03-12 23:05:28 +0800

    SHELL

--- Windows_Smartlog.sh
+++ Windows_Smartlog.sh
@@ -4,7 +4,7 @@ for((i=1;i<="$1";i++)); do
     Slot=$(echo {a..z} |awk '{print $'$i'}')
     echo "$Slot"
 
-    smartctl -a -x /dev/sd"$Slot" > sdsd"$Slot".log
+    smartctl -a -x /dev/sd"$Slot" > sd"$Slot".log
 done
 
 
